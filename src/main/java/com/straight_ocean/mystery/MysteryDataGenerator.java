b45039f548d7643d6905f911a1067459d412b9a3
Straight_Ocean <xzy7885356@163.com>
2025-02-16 15:24:36 +0800

    2025/2/16

---
+++ src/main/java/com/straight_ocean/mystery/MysteryDataGenerator.java
@@ -0,0 +1,11 @@
+package com.straight_ocean.mystery;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MysteryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
